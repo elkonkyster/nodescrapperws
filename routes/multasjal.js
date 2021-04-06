49d3852fd2ab0837c383618166a9cf38eb04fdb6
elkonkyster <31867718+elkonkyster@users.noreply.github.com>
2021-04-06 14:35:09 -0500

    Update multasjal.js

--- routes/multasjal.js
+++ routes/multasjal.js
@@ -2,6 +2,6 @@ const { Router, response } = require('express')
 const {getMultasJal} = require('../controllers/multasjal.controller')
 const router = Router()
 
-router.post('/',getMultasJal)
+router.post('/:placa',getMultasJal)
 
 module.exports = router;
